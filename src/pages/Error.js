b452a720a56ea0f84ad6a983f2807d4f515e709e
missdobis <missdobis@gmail.com>
2020-11-03 11:21:02 +0100

    login page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>Sorry, this page cannot be found.</h3>
+        <Link to="/" className="btn">
+          Back Home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
   place-items: center;
-  background: var(--clr-primary-10);
+  background: var(--clr-primary-9);
   text-align: center;
   h1 {
     font-size: 10rem;
